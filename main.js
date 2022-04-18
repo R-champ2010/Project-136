81dbbf52668a49d3daeea20c599d7b5aa80caa01
R-champ2010 <93424691+R-champ2010@users.noreply.github.com>
2022-04-18 13:00:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+status = "";
+video = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    video.size(380, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
